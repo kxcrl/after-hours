cfb2faa8debda12bf6e8408ef6cf572f8bb7dec4
kixcereal <jeramy.couts@gmail.com>
2014-03-09 15:32:44 -0700

    Solve 99 haskell problems #3 and add types

--- haskell/ninetyNineProblems/1to10.hs
+++ haskell/ninetyNineProblems/1to10.hs
@@ -1,6 +1,12 @@
+myLast :: [a] -> a
 myLast x = last x
 
+myButLast :: [a] -> a
 myButLast x = x !! (length x - 2)
 
+myButLast' :: [t] -> t
 myButLast' [x,_] = x
 myButLast' (_:xs) = myButLast' xs
+
+elementAt :: [a] -> Int -> a
+elementAt list x = list !! (x - 1)
