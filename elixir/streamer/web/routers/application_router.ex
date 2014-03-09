439d856c60781080617af3e90263d93112a06731
kixcereal <jeramy.couts@gmail.com>
2014-03-08 20:38:31 -0800

    Add stream of random numbers

--- elixir/streamer/web/routers/application_router.ex
+++ elixir/streamer/web/routers/application_router.ex
@@ -2,18 +2,29 @@ defmodule ApplicationRouter do
   use Dynamo.Router
 
   prepare do
-    # Pick which parts of the request you want to fetch
-    # You can comment the line below if you don't need
-    # any of them or move them to a forwarded router
     conn.fetch([:cookies, :params])
   end
-
-  # It is common to break your Dynamo into many
-  # routers, forwarding the requests between them:
-  # forward "/posts", to: PostsRouter
 
   get "/" do
     conn = conn.assign(:title, "Welcome to Dynamo!")
     render conn, "index.html"
+  end
+
+  get "/stream" do
+    conn = conn.resp_content_type("text/event-stream")
+    conn = conn.send_chunked(200)
+
+    iterator = Stream.repeatedly(fn -> :random.uniform end)
+
+    Enum.each iterator, fn(element) ->
+      { :ok, conn } = conn.chunk "data: #{element}\n\n"
+      await conn, 200, &on_wake_up(&1, &2), &on_time_out(&1)
+    end
+  end
+
+  defp on_wake_up(_arg1, _arg2) do
+  end
+
+  defp on_time_out(_arg1) do
   end
 end
