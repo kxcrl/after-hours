320663724b475162375745fa32773beebaeefffe
kxcrl <jeramy.couts@gmail.com>
2014-03-25 19:56:18 -0700

    Add basic inputs c challenge

---
+++ c/misc/inputs.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+  /* char firstName[256]; */
+  /* char lastName[256]; */
+  char ch;
+  char name[80];
+  int char_count;
+  int age;
+  int id;
+
+  puts("What is your name?");
+  ch = getchar();
+  char_count = 0;
+  while( (ch != '\n') && (char_count < 80)) {
+    name[char_count++] = ch;
+    ch = getchar();
+  }
+  name[char_count] = '\0';
+
+  puts("How old are you?");
+  scanf("%d", &age);
+
+  puts("What ID would you like?");
+  puts("Please choose 5 numbers.");
+  scanf("%d", &id);
+
+  printf("You are %s.\n", name);
+  printf("You are %d years old, and next year you will be %d.\n", age, age+1);
+  printf("Your user id is %d.\n", id);
+}
