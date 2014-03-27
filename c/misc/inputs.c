736339998cfd8c340db99a1089d507c2f2d8652b
kxcrl <jeramy.couts@gmail.com>
2014-03-27 11:18:49 -0700

    Add friends name, declare input buffer macro

--- c/misc/inputs.c
+++ c/misc/inputs.c
@@ -1,34 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#define INPUTBUFFER 80
 
 int age;
 int id;
 
 char * get_input(){
   char ch;
-  static char input[80];
+  static char input[INPUTBUFFER];
   int char_count;
 
   ch = getchar();
   char_count = 0;
-  while( (ch != '\n') && (char_count < 80)) {
+  while( (ch != '\n') && (char_count < INPUTBUFFER)) {
     input[char_count++] = ch;
     ch = getchar();
   }
   input[char_count] = '\0';
 
+  if(strcmp(input, "exit") == 0){
+    exit(0);
+  }
+
   return input;
 }
 
 int main() {
-  char *name;
+  char *input;
+  char name[INPUTBUFFER];
+  char friend[INPUTBUFFER];
 
   puts("What is your name?");
-  name = get_input();
+  input = get_input();
+  strcpy(name, input);
 
-  if(strcmp(name, "exit") == 0){
-    return 0;
-  }
+  puts("What is your friend's name?");
+  input = get_input();
+  strcpy(friend, input);
 
   puts("How old are you?");
   scanf("%d", &age);
@@ -38,6 +47,7 @@ int main() {
   scanf("%d", &id);
 
   printf("You are %s.\n", name);
+  printf("Your friend is %s.\n", friend);
   printf("You are %d years old, and next year you will be %d.\n", age, age+1);
   printf("Your user id is %d.\n", id);
 }
