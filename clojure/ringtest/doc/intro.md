6303a60e4bba3c15f7946231ce79245926d1fe58
kxcrl <jeramy.couts@gmail.com>
2014-03-29 13:10:32 -0700

    Generate clojure ring test skeleton

---
+++ clojure/ringtest/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ringtest
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ clojure/ringtest/test/ringtest/core_test.clj
@@ -0,0 +1,7 @@
+(ns ringtest.core-test
+  (:require [clojure.test :refer :all]
+            [ringtest.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
