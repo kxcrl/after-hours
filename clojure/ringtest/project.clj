42d51b101fbd0237a379de33627446d55d2f4cba
kxcrl <jeramy.couts@gmail.com>
2014-03-29 13:16:25 -0700

    Add dependencies

--- clojure/ringtest/project.clj
+++ clojure/ringtest/project.clj
@@ -1,6 +1,10 @@
 (defproject ringtest "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Ring test app"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring "1.1.8"]
+                 [compojure "1.1.5"]
+                 [de.ubercode.clostache/clostache "1.3.1"]]
+  :main example.core)
